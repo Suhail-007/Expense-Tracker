44804c028436d0bcb3d8df15299247a01d9085d1
suhuq007@gmail.com <suhuq007@gmail.com>
2023-01-31 21:36:29 +0530

    added expense filter markup

--- code/02-building-a-first-custom-component/src/components/Expenses/Expenses.js
+++ code/02-building-a-first-custom-component/src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter.js';
 
 export default function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2021');
+
+  const onSelectYearHandler = function(selectValue) {
+    setFilteredYear(selectValue);
+  }
+
   return (
     <Card className='expenses'>
-      <h2>Let's get started!</h2>
+      <div>
+        <ExpensesFilter selectedYear={filteredYear} onSelectYear={onSelectYearHandler}/>
+      </div>
       <ExpenseItem title={props.item[0].title} price={props.item[0].amount} date={props.item[0].date} />
       <ExpenseItem title={props.item[1].title} price={props.item[1].amount} date={props.item[1].date} />
       <ExpenseItem title={props.item[2].title} price={props.item[2].amount} date={props.item[2].date} />
